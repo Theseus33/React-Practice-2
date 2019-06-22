9890d10a6bbaa990aa7f03c7fdf4853abacbfb11
Rana Bhattacharyya <ranab33@gmail.com>
2019-06-21 20:21:26 -0400

    checking connections

---
+++ src/helpers.js
@@ -0,0 +1,13 @@
+function helpful() {
+  console.log("I did a super helpful thing");
+}
+
+function sort() {
+  console.log("all sorted");
+}
+
+function sing() {
+  console.log("lal al la");
+}
+
+export { helpful, sort, sing };
